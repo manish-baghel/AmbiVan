8a8eef91bff64ba428d18949443f52964f46c1f4
sunil <sunilssaharan@gmail.com>
2017-09-08 23:28:21 +0530

    made changes to index file

--- backend/controllers/MainController.js
+++ backend/controllers/MainController.js
@@ -1,18 +1,18 @@
 /**
  * Created by manish on 6/9/17.
  */
-var data = require('../Models/db_model');
+//var data = require('../Models/db_model');
 
 module.exports = {
     home:home,
     data:"hello"
 }
-data.getNameFromTable(
-    "SELECT * FROM customers",
+/*data.getNameFromTable(
+    "SELECT * FROM ambivan",
     function(err,result){
         if(err) throw err;
-        console.log(parseIt(result));
-    });
+        console.log(result);
+    });*/
 function home(req, res) {
     res.render('index', {});
 }
---
+++ client/scripts/script.js
@@ -0,0 +1,195 @@
+/* global google */
+/* global _ */
+/**
+ *
+ * Global JavaScript.
+ */
+
+// Google Map
+var map;
+
+// markers for map
+var Markers = [];
+
+// info window
+var infoWindow ;
+
+
+
+function initMap() 
+{
+
+
+
+	var styles = [
+
+        // hide Google's labels
+        {
+            featureType: "all",
+            elementType: "labels",
+            stylers: [
+                {visibility: "off"}
+            ]
+        },
+
+        // hide roads
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+                {visibility: "off"}
+            ]
+        }
+
+    ];
+      
+	map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location has found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+
+    // options for map
+    // https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+/*
+    var options = {
+        center: {lat: 37.4236, lng: -122.1619}, // Stanford, California
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        maxZoom: 14,
+        panControl: true,
+        styles: styles,
+        zoom: 13,
+        zoomControl: true
+    };*/
+
+    // get DOM node in which map will be instantiated
+   // var canvas = $("#map-canvas").get(0);
+
+    // instantiate map
+   // map = new google.maps.Map(canvas, options);
+
+    // configure UI once Google Map is idle (i.e., loaded)
+  // google.maps.event.addListenerOnce(map, "idle", configure);
+
+}  
+
+
+
+
+/**
+ * Configures application.
+ */
+function configure()
+{
+    // update UI after map has been dragged
+    google.maps.event.addListener(map, "dragend", function() {
+        update();
+    });
+
+    // update UI after zoom level changes
+    google.maps.event.addListener(map, "zoom_changed", function() {
+        update();
+    });
+
+    // remove markers whilst dragging
+    google.maps.event.addListener(map, "dragstart", function() {
+        removeMarkers();
+    });
+
+
+    // re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
+    // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
+    document.addEventListener("contextmenu", function(event) {
+        event.returnValue = true; 
+        event.stopPropagation && event.stopPropagation(); 
+        event.cancelBubble && event.cancelBubble();
+    }, true);
+
+    // update UI
+    update();
+
+   
+}
+
+
+
+
+
+
+
+/**
+ * Updates UI's markers.
+ */
+function update() 
+{
+    // get map's bounds
+    var bounds = map.getBounds();
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+
+    // get places within bounds (asynchronously)
+  /*  var parameters = {
+        ne: ne.lat() + "," + ne.lng(),
+        q: $("#q").val(),
+        sw: sw.lat() + "," + sw.lng()
+    };
+ //   $.getJSON("update.js", parameters)
+   // .done(function(data, textStatus, jqXHR)*/
+	 
+
+		var sql1 = "SELECT * FROM ambivan";
+
+		con.query(sql1,function(err,result,fields) {
+		if(err) throw console.log(err.toString());
+		var jsonPretty = JSON.stringify(result,null,2);  
+		console.log("Result: " +jasonPretty);
+		});
+		
+	
+/*
+        // remove old markers from map
+        removeMarkers();
+
+        // add new markers to map
+        for (var i = 0; i < data.length; i++)
+        {
+            addMarker(data[i]);
+        }
+     });
+     .fail(function(jqXHR, textStatus, errorThrown) {
+
+         // log error to browser's console
+         console.log(errorThrown.toString());
+     });*/
+}
+
