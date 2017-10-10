86a2e514c6a3b1297a9cbf845fed67b2c0c6d779
Manish baghel <baghelmanish660@gmail.com>
2017-10-11 05:22:12 +0530

    about us content added

--- backend/Models/user.js
+++ backend/Models/user.js
@@ -5,7 +5,7 @@ var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
 var userSchema = mongoose.Schema({
 
-    local            : {
+        local        : {
         email        : String,
         password     : String
     }
